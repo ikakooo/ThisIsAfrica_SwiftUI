d05f8e9631ec202fcb7f7bf617c5b69c28b17ef2
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2022-12-20 12:08:07 +0400

    progect init

---
+++ ThisIsAfrica/ThisIsAfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThisIsAfricaApp.swift
+//  ThisIsAfrica
+//
+//  Created by IKAKOOO on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ThisIsAfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
