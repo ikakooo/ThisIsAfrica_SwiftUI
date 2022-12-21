1985d51009b9d13aef75296353ef167be1ac005c
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2022-12-21 06:37:35 +0400

    json file decoded and shown with paging

---
+++ ThisIsAfrica/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  ThisIsAfrica
+//
+//  Created by IKAKOOO on 12/20/22.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
