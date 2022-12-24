d31b6317266392494f74f163394c7472acfcdb7f
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2022-12-24 23:47:04 +0400

    integrate mapView

---
+++ ThisIsAfrica/View/InsetFactView.swift
@@ -0,0 +1,36 @@
+//
+//  InsetFactView.swift
+//  ThisIsAfrica
+//
+//  Created by IKAKOOO on 12/24/22.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        
+        GroupBox {
+            TabView(){
+                ForEach(animal.fact, id: \.self) { item in
+                    Text(item)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        }
+    }
+}
+
+struct InsetFactView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        InsetFactView(animal: animals.last!)
+            .previewLayout(.sizeThatFits)
+    }
+}
