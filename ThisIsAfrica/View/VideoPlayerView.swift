fd496687d8fba95a9beedc345485629404bc5cb4
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2022-12-25 05:04:26 +0400

    video player implemented and plays

---
+++ ThisIsAfrica/View/VideoPlayerView.swift
@@ -0,0 +1,44 @@
+//
+//  VideoPlayerView.swift
+//  ThisIsAfrica
+//
+//  Created by IKAKOOO on 12/25/22.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    // MARK: - PROPERTIES
+    
+    var videoSelected: String
+    var videoTitle: String
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")){
+               // Text(videoTitle)
+            }
+            .overlay(
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .padding(.top, 2)
+                .padding(.horizontal,8)
+            ,alignment: .topLeading
+            )
+        }
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle, displayMode: .inline)
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+        }
+    }
+}
