2b30b9201aed6a210542027822f74e3c725aa0d8
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2022-12-28 00:26:30 +0400

    photos shovn in gallery

--- ThisIsAfrica/App/GalleryView.swift
+++ ThisIsAfrica/App/GalleryView.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // GRID ITEMS
+    let gridLayout: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        //GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
-        }
+            VStack(alignment: .center, spacing: 30) {
+                
+                
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.white, lineWidth: 8))
+                
+                
+                // MARK: GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = animal.image
+                            }
+                    }
+                    
+                } //: GRID
+            } //: VSTACK
+            .padding(.vertical, 50)
+            .padding(.horizontal, 10)
+        } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
